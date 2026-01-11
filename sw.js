9a56aa254145e11f5b76db75ab9f469286e72871
wolf_20 <50821986+githubwolf20@users.noreply.github.com>
2026-01-11 17:06:16 +0800

    Add mobile controls and PWA support

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "whitewolf-snake-v1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.svg", "./icon-512.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key))))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
